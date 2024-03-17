593ac50b56bf60a9e76f3241bd67c325c643d142
123vivekr <123vivekr@gmail.com>
2024-03-17 20:04:56 +0530

    Add import test cases for rust

---
+++ test-code-base-rust/import/import.rs
@@ -0,0 +1,7 @@
+mod shapes;
+mod utils;
+
+use shapes::{Circle, Square, Triangle};
+use utils::{function1 as f1, function2 as f2};
+use crate::animals::{Cat, Dog};
+use std::collections::{HashMap, HashSet};
